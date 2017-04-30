dee1edd48fd3374c30244ac805f67a920b644d69
numbqq <hbjsxieqi@163.com>
2017-04-30 15:03:18 +0800

    1. modify build_and_install.sh. 2. add Config.in.

--- busybox-1.17.2/build_and_install.sh
+++ busybox-1.17.2/build_and_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #在rootfs 目录下会生成目录bin、sbin、usr 和文件linuxrc 的内容。这也就是把linux的一些命令放到根文件系统里的特定文件或文件夹里了。
-make CONFIG_PREFIX=/home/nick/nfs/rootfs install -j4
+make CONFIG_PREFIX=/home/nick/nfs_root/rootfs install -j8
